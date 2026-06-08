149f3a7bcd51131847572acf081f99e630c466b7
nicol <nicollyevelysantosrodriguesnic@gamil.com>
2026-06-08 09:28:39 -0300

    segundo commit

---
+++ src/arrays01.java
@@ -0,0 +1,10 @@
+public class arrays01 {
+    public static void main(String[] args){
+        int [] idades = new int[3];
+        idades [0]=21;
+        idades [1]=16;
+        idades [2]=19;
+        System.out.println(idades [2]);
+    }
+
+}
---
+++ src/arrays02.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class arrays02 {
+    static void main(String[] args) {
+        String[] nomes = new String[3];
+        nomes [0] = "sabrina";
+        nomes [1] = "nat";
+        nomes [2] = "debora";
+        for(int i = 0; i < 3; i++){
+            System.out.println(nomes [i]);
+        }
+    }
+}
